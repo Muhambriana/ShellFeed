78d7068c228be09dd3d8b454912d220594b11335
muham <briana.muham@indocyber.id>
2026-02-26 16:42:45 +0700

    feat: update ShellFeedRepositoryTest with success API response validation and upgrade Kotlin version

--- app/src/test/java/com/mshell/shellfeed/source/ShellFeedRepositoryTest.kt
+++ app/src/test/java/com/mshell/shellfeed/source/ShellFeedRepositoryTest.kt
@@ -4,19 +4,24 @@ import com.mshell.shellfeed.core.data.ShellFeedRepositoryImpl
 import com.mshell.shellfeed.core.data.source.Resource
 import com.mshell.shellfeed.core.data.source.remote.ApiResponse
 import com.mshell.shellfeed.core.data.source.remote.RemoteDataSource
-import com.mshell.shellfeed.core.domain.model.NewsDetail
+import com.mshell.shellfeed.utils.DataDummy
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.unmockkAll
+import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class ShellFeedRepositoryTest {
+    val newsResponse = DataDummy.generateDummyNews()
     private val remote = mockk<RemoteDataSource>(relaxed = true)
     private lateinit var repository: ShellFeedRepositoryImpl
 
@@ -26,19 +31,24 @@ class ShellFeedRepositoryTest {
     }
 
     @Test
-    fun `map list`() = runTest {
+    fun `success api response`() = runTest {
         every { remote.getTopHeadlines("us") } returns
                 flowOf(
                     ApiResponse.Success(
-                        listOf(
-                            NewsDetail(),
-                            NewsDetail()
-                        )
+                        DataDummy.generateDummyNews()
                     )
                 )
 
         val result = repository.getTopHeadlines("us").first()
-
         assertTrue(result is Resource.Success)
+
+        val data = (result as Resource.Success).data
+        assertNotNull(data)
+        assertEquals(newsResponse,data)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
     }
 }
