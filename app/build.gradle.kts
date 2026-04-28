cc09f19b65112ff423adf42e9877fc07bfb8f3ea
Muham <muhambriana.31@gmail.com>
2026-04-28 13:35:02 +0700

    feat: update version code to 2 and version name to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,8 +64,8 @@ android {
         applicationId = "com.mshell.feed"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "1.0.0"
 
 
         buildConfigField("String", "API_KEY", apiKey)
