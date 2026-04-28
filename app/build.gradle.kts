2d9f871dc26821f396aac9ac29fd643508f7fe28
Muham <muhambriana.31@gmail.com>
2026-04-28 10:20:58 +0700

    feat: update version name to 0.0.1 and temporarily disable S3 app distribution in CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1"
 
 
         buildConfigField("String", "API_KEY", apiKey)
